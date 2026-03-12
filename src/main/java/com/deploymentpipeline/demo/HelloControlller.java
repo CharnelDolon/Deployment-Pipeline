2ceac94df6f588ae6b4c229511ae5349a7e3b32b
unknown <charnel.dolon@gmail.com>
2026-03-12 16:25:58 -0400

    first commit

---
+++ src/main/java/com/deploymentpipeline/demo/HelloControlller.java
@@ -0,0 +1,12 @@
+package com.deploymentpipeline.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloControlller {
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello from CI/CD pipeline!";
+    }
+}
